5100fcc153a31485da9c27c2ba7c7a38f742a576
Tjark Seemann <tjarkseemann@MacBook-Pro-von-Tjark.local>
2024-10-22 15:35:14 +0200

    refactor

--- app/src/main/java/de/fhkiel/temi/robogguide/MainActivity.kt
+++ app/src/main/java/de/fhkiel/temi/robogguide/MainActivity.kt
@@ -27,7 +27,7 @@ class MainActivity : AppCompatActivity(), OnRobotReadyListener, OnRequestPermiss
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.home_screen)
+        setContentView(R.layout.activity_main)
 
         val tourLengthGroup = findViewById<RadioGroup>(R.id.tourLengthRadioGroup)
         val textLengthGroup = findViewById<RadioGroup>(R.id.textLengthRadioGroup)
