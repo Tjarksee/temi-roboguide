e47aec754a419a65845eba147f6501de4013a663
Tjark Seemann <tjarkseemann@MacBook-Pro-von-Tjark.local>
2024-10-23 12:44:31 +0200

    added some db functions

--- app/src/main/java/de/fhkiel/temi/robogguide/database/DatabaseHelper.kt
+++ app/src/main/java/de/fhkiel/temi/robogguide/database/DatabaseHelper.kt
@@ -9,6 +9,7 @@ import java.io.FileOutputStream
 import java.io.IOException
 import java.io.InputStream
 import java.io.OutputStream
+import java.sql.Types.NULL
 
 class DatabaseHelper(context: Context, private val databaseName: String) : SQLiteOpenHelper(context, databaseName, null, 1) {
 
@@ -108,9 +109,40 @@ class DatabaseHelper(context: Context, private val databaseName: String) : SQLit
         return Pair(columns, primaryKey)
     }
 
+    fun getLocationDataAsJson(mapName: String): Map<String, JSONObject>{
+        val placesId = getSingleValueFromTable("places","SELECT `id` FROM `places` WHERE name=$mapName").toInt()
+        val jsonMap = getTableDataAsJson("locations","SELECT * FROM `locations` WHERE places_id=$placesId")
+        return jsonMap
+    }
+
+    fun getItems(locationId : String): Map<String, JSONObject>{
+        val items = getTableDataAsJson("items", "SELECT * FROM `items` WHERE location_id=$locationId")
+        return items
+    }
+
+
+    fun getTexts (locationId:String,itemId:String,transferId:String) : Map<String, JSONObject>{
+        val textes = getTableDataAsJson("texts", "SELECT * FROM `texts` WHERE location_id=$locationId")
+    }
+
+    fun getSingleValueFromTable(tableName:String, query:String ) : String{
+        var value = ""
+        val(columns, primaryKey) = getTableStructure("items")
+        database?.let { db ->
+            val cursor = db.rawQuery("SELECT ${columns[0]} " + query, arrayOf(null))
+            if (cursor.moveToFirst()) {
+                value = cursor.getString(cursor.getColumnIndexOrThrow(columns[0]))
+            }
+            cursor.close()
+        }
+        if (value == ""){
+            throw IllegalArgumentException("$query was not successful for $tableName .")
+        }
+        return value
+    }
     // Method to read data dynamically and convert it into a JSON Map using the primary key as the key
     @Suppress("unused")
-    fun getTableDataAsJson(tableName: String): Map<String, JSONObject> {
+    fun getTableDataAsJson(tableName: String, query:String): Map<String, JSONObject> {
         val (columns, primaryKey) = getTableStructure(tableName)
 
         if (primaryKey == null) {
@@ -120,7 +152,7 @@ class DatabaseHelper(context: Context, private val databaseName: String) : SQLit
         val jsonMap = mutableMapOf<String, JSONObject>()
 
         database?.let { db ->
-            val cursor = db.rawQuery("SELECT * FROM `$tableName`", null)
+            val cursor = db.rawQuery(query, null)
 
             if (cursor.moveToFirst()) {
                 do {
