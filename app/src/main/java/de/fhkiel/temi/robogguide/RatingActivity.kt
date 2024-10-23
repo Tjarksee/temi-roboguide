62afd5eed81495486aa99e320535dd61afd47337
Tjark Seemann <tjarkseemann@MacBook-Pro-von-Tjark.local>
2024-10-23 10:28:52 +0200

    button functions added

--- app/src/main/java/de/fhkiel/temi/robogguide/RatingActivity.kt
+++ app/src/main/java/de/fhkiel/temi/robogguide/RatingActivity.kt
@@ -1,6 +1,8 @@
 package de.fhkiel.temi.robogguide
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,10 +13,14 @@ class RatingActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_rating)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.rating_view)) { v, insets ->
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.RatingView)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+        findViewById<Button>(R.id.btnContinue).setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
         }
     }
 }
